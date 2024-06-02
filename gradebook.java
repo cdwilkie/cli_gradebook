35e437e6fc767531030918af0edbb950c6f10f22
cdwilkie <cdwilkie@aggies.ncat.edu>
2024-06-02 12:59:52 -0400

    Update Gradebook.java
    Completed Student class and added comments

--- gradebook.java
+++ gradebook.java
@@ -13,12 +13,16 @@ class Student {
     Student() {
         studentName = "";
         studentScores = new HashMap<String, Integer>();
-    }
+    }//end Default Constructor
 
     // getters
+    /**
+     * getName() returns the String value representing a
+     * Student's name, associated with the instance
+     */
     public String getName() {
         return this.studentName;
-    }
+    }//end getName()
 
     /**
      * getScore() returns an integer when called, either returning
@@ -30,10 +34,10 @@ class Student {
     public int getScore(String task) {
         if (this.studentScores.containsKey(task)) {
             return this.studentScores.get(task);
-        }
+        }//end if
         else {
             return -1;
-        }
+        }//end else
     }// end getScore()
 
     /**
@@ -42,15 +46,34 @@ class Student {
      */
     public int getNumScores() {
         return this.studentScores.size();
-    }
+    }//end getNumScores()
 
     // setters
+    /**
+     * setScore() updates the student's grade and associated
+     * task. If the entry for task already exists, a message
+     * notifies the user that an update has occured. If the
+     * task did not exist, the user is notified of the addition.
+     * 
+     * @param task String representation of a task which has received a score
+     * @param score Integer representing score given for a task
+     */
     public void setScore(String task, Integer score) {
         if (this.studentScores.put(task,score) != null) {
             System.out.printf("%s score has been updated to %f\n", task, score);
-        }
+        }//end if
         else {
             System.out.printf("%s score added", task);
-        }
-    }
-}
+        }//end else
+    }//end setScore()
+
+    /**
+     * setName() accepts a string argument and associates
+     * that String as the name for the student instance.
+     * 
+     * @param studentName String representing a student's name
+     */
+    public void setName(String studentName) {
+        this.studentName = studentName;
+    }//end setName()
+}//end Student
